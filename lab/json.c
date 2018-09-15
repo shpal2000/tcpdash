91793ab8ca6c88d573c5d111a9b2e0a6bfa0ca87
shirishpal <shirish.pal@gigamon.com>
2018-09-14 18:32:42 -0700

    json test

--- lab/json.c
+++ lab/json.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <glib-object.h>
 #include <json-glib/json-glib.h>
 #include <json-glib/json-gobject.h>
@@ -7,8 +8,34 @@ int main(int argc, char **argv) {
   JsonNode *root;
   GError *error;
 
-  static const gchar *foo_object_json = "{\"bar\"  : 42,\"baz\"  : true,\"blah\" : \"bravo\"}";
+  static const gchar *foo_object_json = "{\"bar\"  : 42,\"baz\"  : true,\"blah\" : \"bravo\", \"arr\" : [20, 30, 40] }";
 
   root = json_from_string (foo_object_json, &error);
-  error = (GError*)root;
+
+  JsonObject* rootObject = json_node_get_object (root);
+  JsonNode* barNode = json_object_get_member (rootObject, "bar");
+  JsonNode* bazNode = json_object_get_member (rootObject, "baz");
+  JsonNode* blahNode = json_object_get_member (rootObject, "blah");
+  JsonNode* arrNode = json_object_get_member (rootObject, "arr");
+
+  uint64_t barInt = json_node_get_int (barNode); 
+  int bazBool = json_node_get_boolean (bazNode);
+  const char* blahString = json_node_get_string (blahNode);
+  JsonArray *	jsonArr = json_node_get_array (arrNode);
+  uint64_t arrList[10];
+  int jsonArrLength = json_array_get_length(jsonArr);
+
+  int arrCount = 0;
+  for (int i =0; i < 10 && i < jsonArrLength; i++) {
+    arrList[i] = json_array_get_int_element(jsonArr, i);
+    arrCount++;
+  }
+
+  for (int i =0; i < arrCount; i++) {
+    printf ("%" PRIu64 "\n", arrList[i]);
+  }
+
+  printf("bar = %" PRIu64 ", baz=%i, blah = %s\n", barInt, bazBool, blahString);
+
+  return 0;
 }
